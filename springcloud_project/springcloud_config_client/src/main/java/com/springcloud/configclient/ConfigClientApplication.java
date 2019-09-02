179361953cbb921b845be87ffc74d5dcb3d7de8b
mmmm <hzw@uptang.com.cn>
2019-09-02 17:07:43 +0800

    spring cloud 基础配置

---
+++ springcloud_project/springcloud_config_client/src/main/java/com/springcloud/configclient/ConfigClientApplication.java
@@ -0,0 +1,9 @@
+package com.springcloud.configclient;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClientApplication {
+
+}
